d609757e9f1a017b6cba43e50e375f781d11681e
zekphos <59187615+zekphos@users.noreply.github.com>
2024-03-06 18:41:48 +0800

    change

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-define('base_url','54.165.150.218/ets/');
-define('base_app', str_replace('\\','/',__DIR__).'/' );
-define('dev_data',$dev_data);
-define('DB_SERVER',"inf2006-group5-database.cd4s2u4yin1h.us-east-1.rds.amazonaws.com");
-define('DB_USERNAME',"admin");
-define('DB_PASSWORD',"testtest");
-define('DB_NAME',"Expense");
+if(!defined('base_url')) define('base_url','http://54.165.150.218/ets/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"inf2006-group5-database.cd4s2u4yin1h.us-east-1.rds.amazonaws.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"testtest");
+if(!defined('DB_NAME')) define('DB_NAME',"Expense");
 ?>
