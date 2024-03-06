e468c74f383268281af215d0f2e713158aa293b4
zekphos <59187615+zekphos@users.noreply.github.com>
2024-03-06 18:30:29 +0800

    change

--- initialize.php
+++ initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-define('base_url','http://54.165.150.218/ets/');
+define('base_url','54.165.150.218/ets/');
 define('base_app', str_replace('\\','/',__DIR__).'/' );
 define('dev_data',$dev_data);
 define('DB_SERVER',"inf2006-group5-database.cd4s2u4yin1h.us-east-1.rds.amazonaws.com");
