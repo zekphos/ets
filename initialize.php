0a9091fc7bc7d7ad2c1a2a9289d558dc9432c554
zekphos <59187615+zekphos@users.noreply.github.com>
2024-03-06 17:11:54 +0800

    added fileasd

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://18.212.92.230/');
+if(!defined('base_url')) define('base_url','http://54.165.150.218/ets/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"inf2006-group5-database.cd4s2u4yin1h.us-east-1.rds.amazonaws.com");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"testtest");
-if(!defined('DB_NAME')) define('DB_NAME',"inf2006-group5-database");
+if(!defined('DB_NAME')) define('DB_NAME',"Expense");
 ?>
