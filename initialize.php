2322735f5373d3178bda8023d94ccbad1c4d267b
zekphos <59187615+zekphos@users.noreply.github.com>
2024-03-06 17:15:22 +0800

    added file

--- initialize.php
+++ initialize.php
@@ -1,7 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','http://54.165.150.218/ets/');
-if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"inf2006-group5-database.cd4s2u4yin1h.us-east-1.rds.amazonaws.com");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
