eee343cd01e3fe9ab23a70ecd2064bb47ef90aad
zekphos <59187615+zekphos@users.noreply.github.com>
2024-03-06 17:57:15 +0800

    added file

--- initialize.php
+++ initialize.php
@@ -1,5 +1,5 @@
 <?php
-if(!defined('base_url')) define('base_url','http://54.165.150.218/');
+if(!defined('base_url')) define('base_url','http://54.165.150.218/ets/');
 if(!defined('DB_SERVER')) define('DB_SERVER',"inf2006-group5-database.cd4s2u4yin1h.us-east-1.rds.amazonaws.com");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"testtest");
